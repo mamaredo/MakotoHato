045d62b8f002acad7bb889a83cbfcba3d3f520b2
makotty <hal.m.90215@gmail.com>
2021-11-14 18:12:52 +0900

    404:NotFoundページの追加

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import type { FC } from 'react'
+
+// Componet
+import { Layout } from '../components/Layout'
+
+const NotFound: FC = () => {
+  return (
+    <Layout>
+      <h2>ページが見つかりません</h2>
+    </Layout>
+  )
+}
+
+export default NotFound
